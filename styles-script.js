0a3778f9c93168c2781dc1725e1b50a57995fc80
Kara Yount <kara.m.yount@gmail.com>
2016-01-23 12:37:51 -0800

    working html-css portion, starting flask portion

---
+++ styles-script.js
@@ -0,0 +1,6 @@
+var path   = "css";
+var style   = document.createElement( 'link' );
+style.rel   = 'stylesheet';
+style.type  = 'text/css';
+style.href  = 'styles.css';
+document.getElementsByTagName( 'head' )[0].appendChild( style );
