6ff6071e583f69f43b494d4133ef261f15a32a1f
Kara Yount <kara.m.yount@gmail.com>
2016-01-23 14:33:46 -0800

    full working flask exercise

--- application.py
+++ application.py
@@ -1,18 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+app.secret_key = "something-you-would-never-guess-in-14-lifetimes"
 
 
 @app.route("/")
 def index_page():
     """Show an index page."""
 
-    return "<html><body>This is the homepage.</body></html>"
+    return render_template("index.html")
 
-    # Alternately, we could make this a Jinja template in `templates/`
-    # and return that result of rendering this, like:
-    #
-    # return render_template("index.html")
+
+@app.route("/application-form")
+def show_application_form():
+    """Show the application form."""
+
+    return render_template("application-form.html")
+
+
+@app.route("/application", methods=["POST"])
+def show_submitted_application():
+    """Show the submitted application with user-entered data."""
+
+    first_name = request.form.get("first-name")
+    last_name = request.form.get("last-name")
+    desired_salary = request.form.get("salary")
+    job_title = request.form.get("position")
+
+    return render_template("application-response.html",
+                           first=first_name,
+                           last=last_name,
+                           salary=desired_salary,
+                           position=job_title)
 
 
 if __name__ == "__main__":
--- static/styles-script.js
+++ static/styles-script.js
@@ -2,5 +2,5 @@ var path   = "css";
 var style   = document.createElement( 'link' );
 style.rel   = 'stylesheet';
 style.type  = 'text/css';
-style.href  = 'styles.css';
+style.href  = './static/styles.css';
 document.getElementsByTagName( 'head' )[0].appendChild( style );
